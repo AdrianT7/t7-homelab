c23170f9e0d9a5af61e6a9aa194da4dcd9f3a61e
Adrian Turkiewicz <adrianturkiewicz7@gmail.com>
2025-02-06 14:48:12 +0100

    refactor(github_runner): replace token in file for environmental variable

--- github_runner/environments/mgmt/terraform/terraform.tfvars
+++ github_runner/environments/mgmt/terraform/terraform.tfvars
@@ -1,4 +1,3 @@
 proxmox_api_url          = "https://proxmox.home.t7-lab.pl:8006/api2/json"
 proxmox_api_token_id     = "terraform@pam!terraform"
-proxmox_api_token_secret = "secrets/api_key.private"
 proxmox_tls_insecure     = false
--- github_runner/environments/mgmt/terraform/variables.tf
+++ github_runner/environments/mgmt/terraform/variables.tf
@@ -1,4 +1,7 @@
 variable proxmox_api_url          { type = string }
 variable proxmox_api_token_id     { type = string }
-variable proxmox_api_token_secret { type = string }
 variable proxmox_tls_insecure     { type = bool }
+variable proxmox_api_token_secret { 
+  type      = string
+  sensitive = true
+}
