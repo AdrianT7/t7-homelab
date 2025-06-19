8be2361bc4805f63b9d609dd78999346431d6346
AdrianT7 <a.turkiewicz@t7-lab.pl>
2025-06-19 15:32:31 +0200

    feat: deploy zabbix_server

---
+++ zabbix_server/modules/proxmox_vm/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+    }
+  }
+}
+
+resource "proxmox_vm_qemu" "proxmox_instance" {
+  name        = var.instance_name
+  target_node = var.target_node
+  clone       = var.template_name
+  full_clone  = true
+  vm_state    = "running"
+  cpu_type    = "x86-64-v2-AES"
+  sockets     = var.sockets
+  cores       = var.cores
+  memory      = var.memory
+  scsihw      = "virtio-scsi-single"
+  boot        = "order=scsi0"
+
+  disk {
+    slot     = "scsi0"
+    format   = "raw"
+    size     = var.disk_size
+    storage  = var.storage
+  }
+
+  network {
+    id    = 0
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+
+  lifecycle {
+    ignore_changes = [ 
+      disk[0].size
+    ]
+  }
+}
