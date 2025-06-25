161b561776e40657ec254b10a9f16c729ae58628
AdrianT7 <a.turkiewicz@t7-lab.pl>
2025-06-25 21:06:26 +0200

    feat: deploy zabbix-server in docker

--- zabbix_server/environments/mgmt/ansible/execute_playbook.sh
+++ zabbix_server/environments/mgmt/ansible/execute_playbook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook -i inventory.yml prepare_gh_runner_host.yml --ssh-common-args='-o StrictHostKeyChecking=no'
+ansible-playbook -i inventory.yml prepare_zabbix_server.yml --ssh-common-args='-o StrictHostKeyChecking=no' -vvv
