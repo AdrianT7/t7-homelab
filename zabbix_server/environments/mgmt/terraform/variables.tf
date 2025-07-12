398eba6959e00cffdb2b3ed2843ca1344dece0a3
AdrianT7 <a.turkiewicz@t7-lab.pl>
2025-07-12 10:41:33 +0200

    chore: remove no longer needed terraform jobs

--- zabbix_server/environments/mgmt/terraform/variables.tf
+++
@@ -1,7 +0,0 @@
-variable proxmox_api_url          { type = string }
-variable proxmox_api_token_id     { type = string }
-variable proxmox_tls_insecure     { type = bool }
-variable proxmox_api_token_secret { 
-  type      = string
-  sensitive = true
-}
