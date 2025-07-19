a88720ed0553e5fe44a3835c8c0dc50aa3e48a59
AdrianT7 <a.turkiewicz@t7-lab.pl>
2025-07-19 16:46:43 +0200

    chore: delete old tf files and unnecesary jobs

--- github_runner/modules/proxmox_vm/variables.tf
+++
@@ -1,8 +0,0 @@
-variable instance_name { type = string }
-variable target_node   { type = string }
-variable template_name { type = string }
-variable sockets       { type = number }
-variable cores         { type = number }
-variable memory        { type = number }
-variable disk_size     { type = string }
-variable storage       { type = string }
