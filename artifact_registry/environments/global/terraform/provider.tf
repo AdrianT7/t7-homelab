e4f314f7ef932640b6f8d52ef4fc5fc72bbde51a
AdrianT7 <a.turkiewicz@t7-lab.pl>
2025-12-05 23:35:58 +0100

    feat: gcp artifact registry

---
+++ artifact_registry/environments/global/terraform/provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  backend "gcs"{
+    bucket = "t7_gcp_tf_bucket"
+    prefix = "global/gcp_artifact_registry"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.12.0"
+    }
+  }
+}
+
+provider "google" {
+    project = var.project_id
+    region  = var.region
+}
+
+provider "google" {
+  alias   = "impersonated"
+  project = var.project_id
+  region  = var.region
+
+  impersonate_service_account = var.impersonate_service_account
+}
